29c70443fbb61ea0e38aff4cdf1879a924f20189
Fikri Rasyid <fikrirasyid@gmail.com>
2011-10-05 21:57:13 +0700

    Always use template_directory's options theme framework

--- functions.php
+++ functions.php
@@ -8,13 +8,8 @@
  *
 */
 if ( !function_exists( 'optionsframework_init' ) ) {
-    if ( STYLESHEETPATH == TEMPLATEPATH ) {
-            define('OPTIONS_FRAMEWORK_URL', TEMPLATEPATH . '/admin/');
-            define('OPTIONS_FRAMEWORK_DIRECTORY', get_bloginfo('template_directory') . '/admin/');
-    } else {
-            define('OPTIONS_FRAMEWORK_URL', STYLESHEETPATH . '/admin/');
-            define('OPTIONS_FRAMEWORK_DIRECTORY', get_bloginfo('stylesheet_directory') . '/admin/');
-    }
+    define('OPTIONS_FRAMEWORK_URL', TEMPLATEPATH . '/admin/');
+    define('OPTIONS_FRAMEWORK_DIRECTORY', get_bloginfo('template_directory') . '/admin/');
     
     require_once (OPTIONS_FRAMEWORK_URL . 'options-framework.php');
 }
